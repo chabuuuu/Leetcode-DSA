78596ba90a76c2e687534712aed90722f76a96aa
chabuuuu <haphuthinh332004@gmail.com>
2025-02-04 11:23:15 +0700

    36 leetcode

--- Typescript/src/leetcode/09.ts
+++ Typescript/src/leetcode/09.ts
@@ -1,7 +1,7 @@
 export function isPalindrome(x: number): boolean {
     const arrayString: any = x.toString().split('');
     const length = x.toString().length;
-    let mid = length / 2;
+  let mid = length / 2;
     for (let i: number = 0; i < mid; i++) {
         if (Number(arrayString[i]) != Number(arrayString[length - i - 1])) {
             return false;
